bd49e5b963b9dea8b70511a4add3cea97e118cf2
RBreton1 <remy.breton@epitech.eu>
2023-10-28 16:52:01 +0200

    :sparkles: Jump reduce stamina, add sound when player is low on stamina and recover stamina when player died

--- gamemodes/escape_from_the_mod/gamemode/player/sv_stamina.lua
+++ gamemodes/escape_from_the_mod/gamemode/player/sv_stamina.lua
@@ -1,15 +1,24 @@
 util.AddNetworkString("EFTM_player:net:server:updateStamina")
 
+sound.Add({
+	name = "EFTM_player:sound:server:lowStaminaBreath",
+	channel = CHAN_STATIC,
+	volume = 1,
+	level = 60,
+	pitch = { 80, 120 },
+	sound = "player/breathe1.wav"
+})
+
 hook.Add("PlayerInitialSpawn", "EFTM_player:hook:server:setupStats", function(ply)
-	ply.DEFAULTRUN = ply:GetRunSpeed() or 400
-	ply.DEFAULTWALK = ply:GetWalkSpeed() or 200
-	ply.DEFAULTJUMP = ply:GetJumpPower() or 200
+	ply.DEFAULT_RUN = ply:GetRunSpeed() or 400
+	ply.DEFAULT_WALK = ply:GetWalkSpeed() or 200
+	ply.DEFAULT_JUMP = ply:GetJumpPower() or 200
 	ply.STAMINA = 100
 end)
 
 hook.Add("PlayerTick", "EFTM_player:hook:server:manageStamina", function(ply)
     if !ply:Alive() then return end
-	if ply:IsSprinting() && ply:OnGround() && ply:GetVelocity():LengthSqr() >= ply.DEFAULTWALK * ply.DEFAULTWALK then
+	if ply:IsSprinting() && ply:OnGround() && ply:GetVelocity():LengthSqr() >= ply.DEFAULT_RUN * ply.DEFAULT_RUN then
 		ply.STAMINA = math.Clamp(ply.STAMINA - .05, 0, 100)
 		net.Start("EFTM_player:net:server:updateStamina", true)
 			net.WriteFloat(ply.STAMINA)
@@ -20,4 +29,38 @@ hook.Add("PlayerTick", "EFTM_player:hook:server:manageStamina", function(ply)
 			net.WriteFloat(ply.STAMINA)
 		net.Send(ply)
 	end
+
+	if !ply.LOW_STAMINA && ply.STAMINA <= 15 then
+		ply:SetJumpPower(ply.DEFAULT_JUMP * .5)
+		ply:SetRunSpeed(ply.DEFAULT_WALK)
+		ply.LOW_STAMINA = true
+		ply:EmitSound("EFTM_player:sound:server:lowStaminaBreath")
+	elseif ply.LOW_STAMINA && ply.STAMINA > 15 then
+		ply:SetJumpPower(ply.DEFAULT_JUMP)
+		ply:SetRunSpeed(ply.DEFAULT_RUN)
+		ply.LOW_STAMINA = false
+		ply:StopSound("EFTM_player:sound:server:lowStaminaBreath")
+	end
+end)
+
+hook.Add("KeyPress", "EFTM_player:hook:server:jumpManagement", function(ply, key)
+	if !ply:Alive() || ply:InVehicle() || !ply:OnGround() then return end
+
+	if key == IN_JUMP && ply.STAMINA > 0 then
+		ply.STAMINA = math.Clamp(ply.STAMINA - 20, 0, 100)
+		net.Start("EFTM_player:net:server:updateStamina", true)
+			net.WriteFloat(ply.STAMINA)
+		net.Send(ply)
+	end
+end)
+
+hook.Add("PlayerDeath", "EFTM_player:hook:server:resetStamina", function(ply)
+	if ply.STAMINA ~= 100 then
+		ply.STAMINA = 100
+		net.Start("EFTM_player:net:server:updateStamina", true)
+			net.WriteFloat(ply.STAMINA)
+		net.Send(ply)
+		ply.LOW_STAMINA = false
+	end
+	ply:StopSound("EFTM_player:sound:server:lowStaminaBreath")
 end)

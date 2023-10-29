46d07be826cbe3e6e0d701e2f333669d85af80cf
RBreton1 <remy.breton@epitech.eu>
2023-10-29 17:32:34 +0100

    :sparkles: Add system to detect if a new version is available

--- gamemodes/escape_from_the_mod/gamemode/init.lua
+++ gamemodes/escape_from_the_mod/gamemode/init.lua
@@ -7,6 +7,21 @@ AddCSLuaFile("player/cl_stamina.lua")
 include("shared.lua")
 include("player/sv_stamina.lua")
 
-hook.Add("CanPlayerSuicide", "EFTM_gui:hook:server:DisableSuicide", function(ply)
+hook.Add("CanPlayerSuicide", "EFTM:hook:server:DisableSuicide", function(ply)
     return false
 end)
+
+HTTP({
+	failed = function(reason)
+		MsgC(Color(216, 20, 20), string.format("HTTP request failed %s\n", reason))
+	end,
+	success = function(code, body, headers)
+		if code != 200 then
+            return MsgC(Color(216, 20, 20), string.format("HTTP request failed %d\n", code))
+        elseif body != _VERSION then
+            MsgC(Color(201, 168, 25), string.format("A new update is available !\nPlease download the new version\n%s\n", _WEBSITE))
+        end
+	end,
+	method = "GET",
+	url = "https://raw.githubusercontent.com/FlorentWasTaken/Escape_From_The_Mod/main/VERSION"
+})
--- gamemodes/escape_from_the_mod/gamemode/shared.lua
+++ gamemodes/escape_from_the_mod/gamemode/shared.lua
@@ -1,6 +1,9 @@
+_VERSION = "VERSION 0.0.1"
+_WEBSITE = "https://github.com/FlorentWasTaken/Escape_From_The_Mod"
+
 GM.Name     = "Escape From The Mod"
 GM.Author   = "Florent"
-GM.Website  = "https://github.com/FlorentWasTaken/Escape_From_The_Mod"
+GM.Website  = _WEBSITE
 
 DeriveGamemode("sandbox")
 

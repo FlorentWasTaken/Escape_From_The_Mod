7b830056d92c41adba722ad0861cfc14390c4a6d
RBreton1 <remy.breton@epitech.eu>
2023-10-30 21:37:07 +0100

    :art: Change variable name

--- gamemodes/escape_from_the_mod/gamemode/gui/inventory/cl_inventory.lua
+++ gamemodes/escape_from_the_mod/gamemode/gui/inventory/cl_inventory.lua
@@ -1,9 +1,6 @@
-EFTM = EFTM || {}
-EFTM.Config = EFTM.Config || {}
+CreateClientConVar("EFTM_inventory:ccvar:client:openInventory", EFTM.CONFIG.DEFAULT_OPEN_INVENTORY || KEY_I, true, false, "Default key used to open inventory in the Escape From The Mod gamemode")
 
-CreateClientConVar("EFTM_inventory:ccvar:client:openInventory", EFTM.Config.DefaultOpenInventory || KEY_I, true, false, "Default key used to open inventory in the Escape From The Mod gamemode")
-
-local openInventoryKey = cvars.Number("EFTM_inventory:ccvar:client:openInventory", EFTM.Config.DefaultOpenInventory || KEY_I)
+local openInventoryKey = cvars.Number("EFTM_inventory:ccvar:client:openInventory", EFTM.CONFIG.DEFAULT_OPEN_INVENTORY || KEY_I)
 
 hook.Add("PlayerButtonDown", "EFTM_inventory:hook:client:openInventory", function(ply, button)
     if button != openInventoryKey then return end

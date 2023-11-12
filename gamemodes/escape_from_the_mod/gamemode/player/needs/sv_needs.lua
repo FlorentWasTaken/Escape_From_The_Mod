d164065866538a05c6b60238a5d42c1f9b040468
RBreton1 <remy.breton@epitech.eu>
2023-11-12 13:55:02 +0100

    :sparkles: Decrease player health if thirst or hunger is egal to 0

--- gamemodes/escape_from_the_mod/gamemode/player/needs/sv_needs.lua
+++ gamemodes/escape_from_the_mod/gamemode/player/needs/sv_needs.lua
@@ -7,6 +7,35 @@ local function updateNeeds(ply)
     net.Send(ply)
 end
 
+local function dealNeedsDamage(ply, dmg)
+    for k, v in pairs(ply.EFTM.BODY) do
+        if dmg <= 0 then return end
+
+        if v.life - dmg >= 0 then
+            local newHealth = v.life - dmg
+            local life = ply:Health()
+
+            if (newHealth == 0 && v.deadly) || life - newHealth <= 0 then
+                return ply:Kill()
+            end
+            ply.EFTM.BODY[k].life = newHealth
+            ply:SetHealth(life - dmg)
+            break
+        else
+            local newHealth = math.Clamp(v.life - dmg, 0, v.life)
+            local life = ply:Health()
+            local dealedDamage = v.life - newHealth
+
+            if (newHealth == 0 && v.deadly) || life - dealedDamage <= 0 then
+                return ply:Kill()
+            end
+            ply.EFTM.BODY[k].life = newHealth
+            dmg = dmg - v.life
+            ply:SetHealth(life - dealedDamage)
+        end
+    end
+end
+
 hook.Add("PlayerSpawn", "EFTM:hook:server:setupPlayerNeeds", function(ply, _)
     ply.EFTM.NEEDS = {
         ["hunger"] = 100,
@@ -24,7 +53,9 @@ hook.Add("Initialize", "EFTM:hook:server:manageNeeds", function()
             local hunger = ply.EFTM.NEEDS.hunger
             local thirst = ply.EFTM.NEEDS.thirst
 
-            if hunger - rate >= 0 then
+            if hunger == 0 then
+                ply.EFTM.STAMINA_REGEN = 0
+            else if hunger - rate >= 0 then
                 ply.EFTM.NEEDS.hunger = hunger - rate
             else
                 ply.EFTM.NEEDS.hunger = 0
@@ -34,6 +65,22 @@ hook.Add("Initialize", "EFTM:hook:server:manageNeeds", function()
                 ply.EFTM.NEEDS.thirst = thirst - (rate + 1)
             else
                 ply.EFTM.NEEDS.thirst = 0
+            end
+        end
+    end)
+
+    timer.Create("EFTM:timer:server:manageNeeds", 2, 0, function()
+        local players = player.GetAll()
+
+        for _, ply in ipairs(players) do
+            if !IsValid(ply) || !ply.EFTM.NEEDS then continue end
+            local hunger = ply.EFTM.NEEDS.hunger
+            local thirst = ply.EFTM.NEEDS.thirst
+
+            if hunger == 0 && thirst == 0 then
+                dealNeedsDamage(ply, 2)
+            else if hunger == 0 || thirst == 0 then
+                dealNeedsDamage(ply, 1)
             end
         end
     end)

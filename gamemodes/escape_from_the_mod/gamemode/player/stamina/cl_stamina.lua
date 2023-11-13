42cef77c35cbd73b2bded5848c7aec700c505531
RBreton1 <remy.breton@epitech.eu>
2023-11-13 20:00:20 +0100

    :zap: Optimizing stamina network

--- gamemodes/escape_from_the_mod/gamemode/player/stamina/cl_stamina.lua
+++ gamemodes/escape_from_the_mod/gamemode/player/stamina/cl_stamina.lua
@@ -1,5 +1,5 @@
 PLAYER_STAMINA = 100
 
 net.Receive("EFTM_player:net:server:updateStamina", function(len)
-    PLAYER_STAMINA = net.ReadFloat()
+    PLAYER_STAMINA = net.ReadUInt(7)
 end)
--- gamemodes/escape_from_the_mod/gamemode/player/stamina/sv_stamina.lua
+++ gamemodes/escape_from_the_mod/gamemode/player/stamina/sv_stamina.lua
@@ -9,28 +9,37 @@ sound.Add({
 	sound = "player/breathe1.wav"
 })
 
+local function updateStamina(ply)
+	local stamina = math.floor(ply.EFTM.STAMINA)
+
+	if math.floor(ply.EFTM.LAST_STAMINA) == stamina then return end
+
+	ply.EFTM.LAST_STAMINA = stamina
+	net.Start("EFTM_player:net:server:updateStamina")
+		net.WriteUInt(ply.EFTM.STAMINA, 7)
+	net.Send(ply)
+end
+
 hook.Add("PlayerInitialSpawn", "EFTM_player:hook:server:setupStats", function(ply)
 	ply.EFTM = ply.EFTM || {}
 	ply.EFTM.DEFAULT_RUN = ply:GetRunSpeed() || 400
 	ply.EFTM.DEFAULT_WALK = ply:GetWalkSpeed() || 200
 	ply.EFTM.DEFAULT_JUMP = ply:GetJumpPower() || 200
 	ply.EFTM.STAMINA = 100
+	ply.EFTM.LAST_STAMINA = 100
 	ply.EFTM.STAMINA_REGEN = .05
 end)
+
 
 hook.Add("PlayerTick", "EFTM_player:hook:server:manageStamina", function(ply)
     if !ply:Alive() then return end
 
 	if ply:IsSprinting() && ply:OnGround() && ply:GetVelocity():LengthSqr() >= ply.EFTM.DEFAULT_RUN * ply.EFTM.DEFAULT_RUN then
 		ply.EFTM.STAMINA = math.Clamp(ply.EFTM.STAMINA - .05, 0, 100)
-		net.Start("EFTM_player:net:server:updateStamina", true)
-			net.WriteFloat(ply.EFTM.STAMINA)
-		net.Send(ply)
+		updateStamina(ply)
 	elseif !ply:IsSprinting() && ply.EFTM.STAMINA != 100 then
 		ply.EFTM.STAMINA = math.Clamp(ply.EFTM.STAMINA + ply.EFTM.STAMINA_REGEN, 0, 100)
-		net.Start("EFTM_player:net:server:updateStamina", true)
-			net.WriteFloat(ply.EFTM.STAMINA)
-		net.Send(ply)
+		updateStamina(ply)
 	end
 
 	if !ply.EFTM.LOW_STAMINA && ply.EFTM.STAMINA <= 15 then
@@ -52,18 +61,14 @@ hook.Add("KeyPress", "EFTM_player:hook:server:jumpManagement", function(ply, key
 
 	if key == IN_JUMP && ply.EFTM.STAMINA > 0 then
 		ply.EFTM.STAMINA = math.Clamp(ply.EFTM.STAMINA - 20, 0, 100)
-		net.Start("EFTM_player:net:server:updateStamina", true)
-			net.WriteFloat(ply.EFTM.STAMINA)
-		net.Send(ply)
+		updateStamina(ply)
 	end
 end)
 
 hook.Add("PlayerDeath", "EFTM_player:hook:server:resetStamina", function(ply)
 	if ply.EFTM.STAMINA != 100 then
 		ply.EFTM.STAMINA = 100
-		net.Start("EFTM_player:net:server:updateStamina", true)
-			net.WriteFloat(ply.EFTM.STAMINA)
-		net.Send(ply)
+		updateStamina(ply)
 		ply.EFTM.LOW_STAMINA = false
 	end
 	ply.EFTM.STAMINA_REGEN = .05

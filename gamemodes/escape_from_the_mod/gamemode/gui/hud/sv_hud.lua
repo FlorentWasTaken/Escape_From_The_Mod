df6e33896d509654d3009391c61166df2458d2c4
RBreton1 <remy.breton@epitech.eu>
2023-12-06 20:07:06 +0100

    :sparkles: Add broadcast notifiction function

--- gamemodes/escape_from_the_mod/gamemode/gui/hud/sv_hud.lua
+++ gamemodes/escape_from_the_mod/gamemode/gui/hud/sv_hud.lua
@@ -9,3 +9,13 @@ function notifyPlayer(ply, message, messageType)
         net.WriteUInt(messageType, 2)
     net.Send(ply)
 end
+
+function notifyBroadcast(message, messageType)
+    if not message or not messageType then return end
+    if type(message) ~= "string" or type(messageType) ~= "number" then return end
+
+    net.Start("EFTM_gui:net:server:notify")
+        net.WriteString(message)
+        net.WriteUInt(messageType, 2)
+    net.Broadcast()
+end

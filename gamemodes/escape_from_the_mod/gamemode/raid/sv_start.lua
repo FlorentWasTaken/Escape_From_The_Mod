911827b67d4a834d833f35759b44314664b42483
RBreton1 <remy.breton@epitech.eu>
2023-12-11 20:58:53 +0100

    :sparkles: Manage players spawn point

--- gamemodes/escape_from_the_mod/gamemode/raid/sv_start.lua
+++ gamemodes/escape_from_the_mod/gamemode/raid/sv_start.lua
@@ -24,25 +24,68 @@ local function clearRaid()
     bringRaidPlayers()
 end
 
+local function countAvailablePlayer()
+    local count = 0
+
+    for k, v in ipairs(player.GetAll()) do
+        if not p.EFTM.HAS_BEEN_INIT then continue end
+        count = count + 1
+    end
+    return count
+end
+
+local function managePlayerSpawn(players, spawns, extracts, spawnCount)
+    local currentTime = CurTime()
+    local currentZone = 1
+    local waitingPlayers = {}
+
+    for _, p in ipairs(players) do
+        if not p.EFTM.HAS_BEEN_INIT or not p.EFTM.IN_HIDEOUT then continue end
+        if not p:Alive() then p:Spawn() end
+
+        for k, v in ipairs(spawns[currentZone]) do
+            if v.LastSpawn and v.LastSpawn > currentTime - 10 then continue end
+
+            spawns[currentZone].LastSpawn = currentTime
+            p.EFTM.IN_HIDEOUT = false
+            p.EFTM.EXTRACT_ZONE = currentZone
+            net.Start("EFTM_raid:net:server:startRaid")
+                net.WriteString(extracts)
+            net.Send(p)
+            currentZone = currentZone + 1
+            p:SetPos(Vector(v.x, v.y, v.z))
+            p:SetEyeAngles(Angle(0, v.w, 0))
+
+            if currentZone > spawnCount then currentZone = 1 end
+            break
+        end
+        if p.EFTM.IN_HIDEOUT then table.insert(waitingPlayers, p) end
+    end
+    return waitingPlayers
+end
+
 function startRaid()
+    if not EFTM.CONFIG.MAP.spawns then return end
+    if not EFTM.CONFIG.MAP.extracts then return end
+
     local extracts = util.TableToJSON(EFTM.CONFIG.MAP.extracts)
+    local spawns = table.Copy(EFTM.CONFIG.MAP.spawns)
+    local spawnCount = #spawns
+    local waitingPlayers = {}
 
     clearRaid()
     timer.Pause("EFTM_raid:timer:server:raidStartup")
     IS_RAID_STARTED = true
-    for _, p in ipairs(player.GetAll()) do
-        if not p.EFTM.HAS_BEEN_INIT then continue end
-        if not p:Alive() then p:Spawn() end
+    waitingPlayers = managePlayerSpawn(player.GetAll(), spawns, extracts, spawnCount)
 
-        p.EFTM.IN_HIDEOUT = false
-        net.Start("EFTM_raid:net:server:startRaid")
-            net.WriteString(extracts)
-        net.Send(p)
-        -- TODO : TP player to spawn point
-    end
+    if #waitingPlayers == 0 then return end
+    timer.Create("EFTM_raid:timer:server:waitingPlayersSpawn", 12, 0, function()
+        waitingPlayers = managePlayerSpawn(player.GetAll(), spawns, extracts, spawnCount)
+        if #waitingPlayers == 0 then timer.Remove("EFTM_raid:timer:server:waitingPlayersSpawn") end
+    end)
 end
 
-hook.Add("Initialize", "EFTM:hook:server:raidStartup", function()
+hook.Add("Initialize", "EFTM_raid:hook:server:raidStartup", function()
     local count = 0
 
     timer.Create("EFTM_raid:timer:server:raidStartup", 10, 0, function()

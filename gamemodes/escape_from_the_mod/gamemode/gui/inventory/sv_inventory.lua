00fbb68aa8fbb4d9c512f1cfcf2deef78a14ca6f
RBreton1 <remy.breton@epitech.eu>
2024-01-02 20:55:55 +0100

    :sparkles: #2 Setup players inventory table

--- gamemodes/escape_from_the_mod/gamemode/gui/inventory/sv_inventory.lua
+++ gamemodes/escape_from_the_mod/gamemode/gui/inventory/sv_inventory.lua
@@ -1,14 +1,32 @@
 local _player = FindMetaTable("Player")
 
+Slot = {}
+
+function Slot:new(_type)
+    local obj = {}
+
+    if _type ~= "X" and _type ~= "Y" then return nil end
+
+    setmetatable(obj, self)
+    self.__index = self
+    self.type = _type -- correspond to X or Y
+    self.replacement = nil -- correspond to the item pos in ITEMS table
+    return obj
+end
+
 function _player:getItemByPos(pos)
-    return self.EFTM.INVENTORY[pos]
+    return self.EFTM.INVENTORY.ITEMS[pos]
 end
 
 function _player:removeItem(item)
-    table.RemoveByValue(self.EFTM.INVENTORY, item)
+    table.RemoveByValue(self.EFTM.INVENTORY.ITEMS, item)
 end
 
 hook.Add("PlayerInitialSpawn", "EFTM:hook:server:loadPlayerItems", function(ply, _)
     ply.EFTM = ply.EFTM or {}
-    ply.EFTM.INVENTORY = {}
+    ply.EFTM.INVENTORY = {
+        BAG = {ITEMS = {}, SHAPE = {}},
+        POCKETS = {ITEMS = {}, SHAPE = {{Slot:new("X"), Slot:new("Y"), Slot:new("X"), Slot:new("Y")}}}, -- X or Y = 1 square but X and X = rectangle
+        RIG = {ITEMS = {}, SHAPE = {}}
+    }
 end)
